af4452c32bcee09740e731223a10c0ca9260e97e
DarkAaron1 <felipe.burgos1@virginiogomez.cl>
2025-12-15 00:50:28 -0300

    Almacenamiento y Evento de Pedido/Comandas

---
+++ app/Events/NuevoPedidoCreado.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Pedido;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class NuevoPedidoCreado
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+
+    public $pedido;
+
+
+    public function __construct(Pedido $pedido)
+    {
+        $this->pedido = $pedido->load('comanda');
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new Channel('cocina'),
+        ];
+    }
+
+    public function broadcastAs(): string
+    {
+        return 'NuevoPedidoCreado';
+    }
+}
